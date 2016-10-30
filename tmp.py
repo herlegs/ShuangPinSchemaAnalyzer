6c1d714fb022f1b7c3732ab14ed36c5c4b3e765e
Yuchen Zhang <Yuchen Zhang>
2016-10-30 19:21:45 +0800

    add get key presses logic

---
+++ tmp.py
@@ -0,0 +1,16 @@
+tmp = {
+	"consonants": {
+		"c": "c",
+		"ch": "i",
+	},
+	"vowels": {
+		"a": "a",
+		"ao": "c"
+	}
+}
+
+def is_vowel_word(pinyin):
+	return
+
+def get_key_presses(pinyin):
+	return
